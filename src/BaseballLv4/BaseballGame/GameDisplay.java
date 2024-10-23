60d80538c93cb5212f735cabc8bea6f7e0eeffa6
zunmin <2unmin4030@gmail.com>
2024-10-23 15:51:06 +0900

    Fix. 난이도(3~5)입력 이외의 값들이 입력이 되는 예외 처리 구현

--- src/BaseballLv4/BaseballGame/GameDisplay.java
+++ src/BaseballLv4/BaseballGame/GameDisplay.java
@@ -15,8 +15,10 @@ public class GameDisplay {
         History history = new History();
         Scanner sc = new Scanner(System.in);
         String num;
+        SettingLevel settingLevel = new SettingLevel();
+        String level = settingLevel.LevelCheck("3");
+        String difficult ;
         Ball ball = null;
-        String level = "";
         while (check) {
             System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
             System.out.println("0. 자릿수 설정 1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
@@ -28,19 +30,22 @@ public class GameDisplay {
                 switch (startExit) {
                     case 0:
                         System.out.println("설정하고자 하는 자리수를 입력하세요");
-                        String difficult= sc.nextLine();
-                        SettingLevel settingLevel = new SettingLevel();
+                        difficult= sc.nextLine();
+                        if(Integer.parseInt(difficult)>5 || Integer.parseInt(difficult)<3){
+                            System.out.println("설정하실 난이도가 올바르지 않습니다.");
+                            answer=-1;
+                            break;
+                        }
                         level = settingLevel.LevelCheck(difficult);
-                        ball = new Ball(level);
                         answer=-1;
                         break;
 
                     case 1:
                         //기능
-
+                        ball = new Ball(level);
                         System.out.println("숫자를 입력해주세요");
                         num = sc.nextLine();
-                        Validator[] validators = {new NumberCheck(), new DigitsCheck(level), new DuplicateCheck(level), new ExceedOneCheck()};
+                        Validator[] validators = {new NumberCheck(), new DuplicateCheck(level), new DigitsCheck(level), new ExceedOneCheck()};
                         for (Validator validator : validators) {
                             if (!validator.validation(num)) {
                                 answer = -1;
